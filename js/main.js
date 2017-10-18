a7ac723260c73f69136a347b82e0df5693e255e9
frank-ec <franklynlara@hotmail.com>
2017-10-18 15:25:39 -0500

    Antes de PHP

--- js/main.js
+++ js/main.js
@@ -55,9 +55,7 @@ var api= 'AIzaSyBMe5zzwFKSjWktOY1BaJz79oARGaC4k9Q';
     		var camisas = document.getElementById('camisa_evento');
     		var etiquetas = document.getElementById('etiquetas');
 
-
-
-
+            if (document.getElementById('calcular')) {
     		calcular.addEventListener('click',calcularMontos);
     		
             pase_dia.addEventListener('blur',mostrarDias);
@@ -188,6 +186,8 @@ var api= 'AIzaSyBMe5zzwFKSjWktOY1BaJz79oARGaC4k9Q';
                       document.getElementById(diasElegidos[i]).style.display = 'block';
                     }
               }
+
+             } 
   });
   
 })();
@@ -197,6 +197,26 @@ $(function(){
 
     //  Lettering
     $('.nombre-sitio').lettering();
+
+    // Meńu fijo
+        var windowHeight = $(window).height();
+        var barraAltura = $('.barra').innerHeight();
+        
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top':barraAltura+'px'});
+        }else{
+             $('.barra').removeClass('fixed');
+             $('body').css({'margin-top': '0px'});
+        }
+     });   
+
+    // Menu Responsive
+    $('.menu-movil').on('click', function(){
+        $('.navegacion-principal').slideToggle();
+    });
     
     // Programa de Conferencias
     $('div.ocultar').hide(); //Oculta Talleres, Conferencias y Seminarios
@@ -213,10 +233,20 @@ $(function(){
     });
 
    // Animaciones para los números 
-      $('.resumen-evento li:nth-child(1) p').animateNumber({ number:6},1200);  
-      $('.resumen-evento li:nth-child(2) p').animateNumber({ number:15},1200);  
-      $('.resumen-evento li:nth-child(3) p').animateNumber({ number:3},1500);  
-      $('.resumen-evento li:nth-child(4) p').animateNumber({ number:9},1500);  
+      var resumenLista = jQuery('.resumen-evento'); 
+      if (resumenLista.length > 0) {
+        $('.resumen-evento').waypoint(function(){
+            $('.resumen-evento li:nth-child(1) p').animateNumber({ number:6},1200);  
+            $('.resumen-evento li:nth-child(2) p').animateNumber({ number:15},1200);  
+            $('.resumen-evento li:nth-child(3) p').animateNumber({ number:3},1500);  
+            $('.resumen-evento li:nth-child(4) p').animateNumber({ number:9},1500); 
+        },{
+            offset: '60%'
+        }
+        )
+      }
+
+       
 
    // Cuenta regresiva
       $('.cuenta-regresiva').countdown('2017/10/21 09:00:00', function(event){
