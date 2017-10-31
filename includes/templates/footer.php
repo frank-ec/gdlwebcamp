c203c81a296a8c16a274b928c7af1529886474f9
frank-ec <franklynlara@hotmail.com>
2017-10-31 15:39:05 -0500

    Usando require_once

---
+++ includes/templates/footer.php
@@ -0,0 +1,56 @@
+           <footer class="site-footer">
+                <div class="contenedor clearfix">
+                    <div class="footer-informacion">
+                        <h3>Sobre <span>gdlwebcamp</span></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel in cum fugiat voluptate dignissimos soluta labore qui distinctio aperiam ea accusantium, nihil optio quibusdam dolorum facilis doloremque, enim assumenda explicabo!</p>
+                    </div>
+                    <div class="ultimos-tweets">
+                        <h3>ültimos <span>tweets</span></h3>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non</li>
+                        </ul>
+                    </div>
+                    <div class="footer-informacion">
+                        <h3>Redes <span>sociales</span></h3>
+                         <nav class="redes-sociales">
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </nav>
+                    </div>
+                                       
+                </div>
+                <p class="copyright">
+                    Todos los derechos Reservados TIC's DD1710 - 2017
+                </p>
+            </footer>
+
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/jquery.animateNumber.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.lettering.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+
+         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMe5zzwFKSjWktOY1BaJz79oARGaC4k9Q&callback=initMap"
+    async defer></script>
+
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="no-js" lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <header class="site-header">
+            <div class="hero">
+                <div class="contenido-header">
+                    <nav class="redes-sociales">
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </nav>
+                    <div class="informacion-evento">
+                        <div class="clearfix">
+                            <p class="fecha"><i class="fa fa-calendar" aria-hidden="true"></i>10-12 Dic</p>
+                            <p class="ciudad"><i class="fa fa-map-marker" aria-hidden="true"></i>Cayambe, EC</p>
+                        </div>
+                       
+                        <h1 class="nombre-sitio">HB Cayambe</h1>
+                        <p class="slogan">Atendemos con <span>calidad y calidéz</span> </p>
+                   </div> <!--Fin informacion evento -->
+                  
+
+                </div>
+                
+            </div> <!--Fin hero-->
+
+         </header>
+
+         <div class="barra">
+            <div class="contenedor clearfix">
+                <div class="logo">
+                <a href="index.php">
+                    <img src="img/logo.svg" alt="logo gdlwebcamp">
+                </a>    
+                </div>
+                
+                <div class="menu-movil">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <nav class="navegacion-principal clearfix">
+                    <a href="conferencia.php">Ferias de Salud</a>
+                    <a href="#">Calendarios</a>
+                    <a href="invitados.php">Invitados</a>
+                    <a href="registro.php">Reserva Turnos</a>
+                </nav>
+
+            </div> <!--fin Contenedor -->
+
+        </div> <!--fin Barra -->
