9b3861118d5bbf21840143084c825a2668bc1654
frank-ec <franklynlara@hotmail.com>
2017-12-15 13:48:00 -0500

    Do while en Index

--- index1.php
+++ index1.php
@@ -13,90 +13,90 @@
                 </video>
             </div> <!-- contenedor video -->
 
-            <div class="contenido-programa">
+         <div class="contenido-programa">
                 <div class="contenedor">
                     <div class="programa-evento">
                         <h2>Programa del Evento</h2>
-                         <?php
-                            try{
+                        
+                        <?php 
+                            try {
                               require_once('includes/funciones/bd_conexion.php');
-                              $sql = "SELECT * FROM `categoria_evento` " ;
+                              $sql = "SELECT * FROM `categoria_evento` ";
                               $resultado = $conn->query($sql);
-                              }catch (Exception $e){
-                              $error = $e->getMessage();  
-                              }
+                            } catch (Exception $e) {
+                              $error = $e->getMessage();
+                            }
                          ?>
-
                         <nav class="menu-programa">
-                           <?php while ($cat = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
-                                <?php $categoria = $cat['cat_evento']; ?>   
-                            <a href="#<?php echo strtolower($categoria);?> ">
-                                <i class="fa  <?php echo $cat['icono'] ?> " aria-hidden="true"></i> <?php echo $categoria ?>
-                            </a>
-                           <?php } ?> 
+                          <?php while($cat = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
+                            <?php $categoria = $cat['cat_evento']; ?>
+                                <a href="#<?php echo strtolower($categoria) ?>">
+                                      <i class="fa <?php echo $cat['icono'] ?>" aria-hidden="true"></i> <?php echo $categoria ?>
+                                </a>
+                          <?php } ?>
                         </nav>
-                            <?php
-                                try{
-                                  require_once('includes/funciones/bd_conexion.php');
-                                  $sql = "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` " ;
-                                  $sql .= "FROM `eventos` " ;
-                                  $sql .= "INNER JOIN `categoria_evento` " ;
-                                  $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
-                                  $sql .= "INNER JOIN `invitados` " ;
-                                  $sql .= "ON eventos.id_inv=invitados.invitado_id ";
-                                  $sql .= "AND eventos.id_categoria = 1";
-                                  $sql .= "ORDER BY `evento_id` LIMIT 2;";
-                                  $sql = "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` " ;
-                                  $sql .= "FROM `eventos` " ;
-                                  $sql .= "INNER JOIN `categoria_evento` " ;
-                                  $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
-                                  $sql .= "INNER JOIN `invitados` " ;
-                                  $sql .= "ON eventos.id_inv=invitados.invitado_id ";
-                                  $sql .= "AND eventos.id_categoria = 2";
-                                  $sql .= "ORDER BY `evento_id` LIMIT 2;";
-                                  $sql = "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` " ;
-                                  $sql .= "FROM `eventos` " ;
-                                  $sql .= "INNER JOIN `categoria_evento` " ;
-                                  $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
-                                  $sql .= "INNER JOIN `invitados` " ;
-                                  $sql .= "ON eventos.id_inv=invitados.invitado_id ";
-                                  $sql .= "AND eventos.id_categoria = 3";
-                                  $sql .= "ORDER BY `evento_id` LIMIT 2;";
-                                  } catch (Exception $e){
-                                  $error = $e->getMessage();  
-                                  }
-                             ?>   
-                            <?php $conn->multi_query($sql); ?>
-                            <?php
-                                  do {
-                                      $resultado = $conn->store_result();
-                                      $row = $resultado->fetch_all(MYSQLI_ASSOC); ?>
-                                        
-                                        <?php $i = 0;  ?>
-                                        <?php foreach ($row as $evento):?>
-                                          <?php if($i % 2 == 0 ) { ?>
+                        
+                        <?php 
+                            try {
+                              require_once('includes/funciones/bd_conexion.php');
+                              $sql = "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` ";
+                              $sql .= "FROM `eventos` ";
+                              $sql .= "INNER JOIN `categoria_evento` ";
+                              $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
+                              $sql .= "INNER JOIN `invitados` ";
+                              $sql .= "ON eventos.id_inv=invitados.invitado_id ";
+                              $sql .= "AND eventos.id_cat_evento = 1 ";
+                              $sql .= "ORDER BY `evento_id` LIMIT 2;";
+                              $sql .= "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` ";
+                              $sql .= "FROM `eventos` ";
+                              $sql .= "INNER JOIN `categoria_evento` ";
+                              $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
+                              $sql .= "INNER JOIN `invitados` ";
+                              $sql .= "ON eventos.id_inv=invitados.invitado_id ";
+                              $sql .= "AND eventos.id_cat_evento = 2 ";
+                              $sql .= "ORDER BY `evento_id` LIMIT 2;";
+                              $sql .= "SELECT `evento_id`, `nombre_evento`, `fecha_evento`, `hora_evento`, `cat_evento`, `nombre_invitado`, `apellido_invitado` ";
+                              $sql .= "FROM `eventos` ";
+                              $sql .= "INNER JOIN `categoria_evento` ";
+                              $sql .= "ON eventos.id_cat_evento=categoria_evento.id_categoria ";
+                              $sql .= "INNER JOIN `invitados` ";
+                              $sql .= "ON eventos.id_inv=invitados.invitado_id ";
+                              $sql .= "AND eventos.id_cat_evento = 3 ";
+                              $sql .= "ORDER BY `evento_id` LIMIT 2;";
+                            } catch (Exception $e) {
+                              $error = $e->getMessage();
+                            }
+                         ?>
+                        <?php $conn->multi_query($sql); ?>
+                        
+                        <?php 
+                            do {
+                                $resultado = $conn->store_result();
+                                $row = $resultado->fetch_all(MYSQLI_ASSOC);    ?>
+                                
+                                <?php $i = 0; ?>
+                                <?php foreach($row as $evento): ?>
+                                  <?php if($i % 2 == 0) { ?>
                                     <div id="<?php echo strtolower($evento['cat_evento']) ?>" class="info-curso ocultar clearfix">
-                                          <?php } ?>
-                                        <div class="detalle-evento">
-                                            <h3><?php echo utf8_encode($evento['nombre_evento']) ?> </h3>
-                                            <p><i class="fa fa-clock-o" aria-hidden="true"></i><?php echo $evento['hora_evento'] ?> </p>
-                                            <p><i class="fa fa-calendar" aria-hidden="true"></i><?php echo $evento['fecha_evento'] ?></p>
-                                            <p><i class="fa fa-user" aria-hidden="true"></i><?php echo $evento['nombre_invitado'] ." ".$evento['nombre_invitado'] ;?></p>
-                                        </div>
-                                        
-                                        <a href="calendario.php" class="button float-rigth" >Ver todos</a>
-                                    <?php if($i % 2 == 1 ) : ?>    
-                                    </div> <!-- Talleres -->
-                                    <?php endif; ?>
-                                    <?php $i++ ; ?>
-                                    <?php endforeach; ?>
-                                    <?php $resultado->free(); ?>
-                                  
-                             <?php     }while ($conn->more_results() && $conn->next_result()); ?>
-                         
-                    </div> <!-- programa-evento -->
-                </div> <!-- contenedor  -->
-            </div> <!-- contenido-programa  -->
+                                  <?php } ?>
+                                          <div class="detalle-evento">
+                                              <h3><?php echo html_entity_decode($evento['nombre_evento']) ?></h3>
+                                              <p><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $evento['hora_evento']; ?></p>
+                                              <p><i class="fa fa-calendar" aria-hidden="true"></i> <?php echo $evento['fecha_evento']; ?></p>
+                                              <p><i class="fa fa-user" aria-hidden="true"></i> <?php echo $evento['nombre_invitado'] . " " .  $evento['apellido_invitado']; ?></p>
+                                          </div>
+                                  <?php if($i % 2 == 1): ?>
+                                        <a href="calendario.php" class="button float-right">Ver todos</a>
+                                    </div> <!--#talleres-->
+                                  <?php endif; ?>
+                                <?php $i++; ?>
+                                <?php endforeach; ?>
+                                <?php $resultado->free(); ?>
+                          <?php  } while ($conn->more_results() && $conn->next_result());?>
+                  
+                    </div> <!--.programa-evento-->
+                </div> <!--.contenedor-->
+            </div><!--.contenido-programa-->  
         </section> <!-- seccion programa -->
 
  <?php include_once "includes/templates/invitados.php" ;?> 
